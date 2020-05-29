2658016a81ea557c3952ce6d0cdc1aadce115d3f
Andrew Wiedenmann <andrewwiedenmann1@gmail.com>
2020-05-29 17:15:35 -0500

    add comments to js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,4 +1,10 @@
+//A simple script to update the content of an html element with facts about me
+//depending on which element is clicked by a user
+//Author: Andrew Wiedenmann
 "use strict";
+
+//A map of displayKey values mapped to html string content. When an element is clicked, the string corresponding to that
+//displayKey value is displayed on the webpage
 let popupContentMap = new Map();
 
 let PHILOSOPHY_STR_CONTENT = "Right now, I'm reading  'On the Aesthetic Education of Man' by Friedrich Schiller. It's where I found the quote at the top of the page.";
@@ -6,9 +12,7 @@ let BOARD_GAMES_STR_CONTENT = "In quarantine, I've been playing Betrayal and Cat
 let MOVIES_STR_CONTENT = "I have been on a horror movie kick recently - I just watched Hush, The Ring, and Netflix's The Platform";
 let TV_SHOWS_STR_CONTENT = "I enjoy Parks and Rec, M*A*S*H, King of the Hill, and I recently started watching Aziz Ansari's Master of None";
 let MUSIC_STR_CONTENT = "Top 5: Dunno - Mac Miller, Summerhouse - Kota the Friend, Fishing for Fishies - King Gizzard, Volcano - Jimmy Buffett, Woods - Bon Iver";
-
 let DRONES_STR_CONTENT = "On campus, I am a research assistant to a PhD student working on drone swarms. During the school year, I work about 10-15 hours a week in the lab coding, gathering data, or helping with data visualization. I am also a member of my school's UAV robotics club, where I work mostly with control systems";
-
 let CODING_STR_CONTENT = "I have taken two college classes in Java about algorithms and data structures. In my work as a research assistant, I code in C++ and python.";
 let WEB_DEV_STR_CONTENT = "And, as part of this project, I learned HTML, CSS and Javascript";
 let MACHINE_LEARNING_STR_CONTENT = "Recently, I have been learning tensorflow to apply it to my research project.";
@@ -25,10 +29,9 @@ popupContentMap.set("webDev", WEB_DEV_STR_CONTENT);
 
 document.addEventListener("click", (event) => respondToEvent(event));
 
-function respondToEvent(event){
+function respondToEvent(event) {
     let clickDisplayKey = event.target.getAttribute("displayKey");
-    if(clickDisplayKey == null)
-    {
+    if (clickDisplayKey == null) {
         return;
     }
     let sectionId = event.target.parentNode.parentNode.parentNode.id;
