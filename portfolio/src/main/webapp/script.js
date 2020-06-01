3cd67452c15e136bfd646099ab1cf7a03a0c6de2
Andrew Wiedenmann <andrewwiedenmann1@gmail.com>
2020-06-01 10:01:39 -0500

    update js style

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -9,16 +9,16 @@ let coll = document.getElementsByClassName("collapsible");
 let i;
 
 for (i = 0; i < coll.length; i++) {
-  // Add onclick functionality to every item in coll
-  coll[i].addEventListener("click", function() {
-    // invert active (active -> inactive or inactive -> inactive)
-    this.classList.toggle("active");
-    let content = this.nextElementSibling;
-    // If showing, unshow. Else, if not showing, show
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+    // Add onclick functionality to every item in coll
+    coll[i].addEventListener("click", function() {
+        // invert active (active -> inactive or inactive -> inactive)
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        // If showing, unshow. Else, if not showing, show
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
 }
