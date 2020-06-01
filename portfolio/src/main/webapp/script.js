8f8d3121df30910b8f338538b73efc7c51f8f140
Andrew Wiedenmann <andrewwiedenmann1@gmail.com>
2020-06-01 09:35:33 -0500

    Add comments to js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,12 +1,20 @@
+/**
+ A script that adds on click functionality to a set of buttons on my webpage
+ @author Andrew Wiedenmann
+ */
 "use strict";
 
+//Get all buttons. coll is an array of html elements
 let coll = document.getElementsByClassName("collapsible");
 let i;
 
 for (i = 0; i < coll.length; i++) {
+  //Add onclick functionality to every item in coll
   coll[i].addEventListener("click", function() {
+    //invert active (active -> inactive or inactive -> inactive)
     this.classList.toggle("active");
     let content = this.nextElementSibling;
+    //If showing, unshow. Else, if not showing, show
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
