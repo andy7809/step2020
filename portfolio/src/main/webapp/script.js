e7b67aa4d2cdb4b45195ccc210b5e2ec527a7f6e
Andrew Wiedenmann <andrewwiedenmann1@gmail.com>
2020-06-01 09:51:07 -0500

    Add space after // in js comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -4,17 +4,17 @@
  */
 "use strict";
 
-//Get all buttons. coll is an array of html elements
+// Get all buttons. coll is an array of html elements
 let coll = document.getElementsByClassName("collapsible");
 let i;
 
 for (i = 0; i < coll.length; i++) {
-  //Add onclick functionality to every item in coll
+  // Add onclick functionality to every item in coll
   coll[i].addEventListener("click", function() {
-    //invert active (active -> inactive or inactive -> inactive)
+    // invert active (active -> inactive or inactive -> inactive)
     this.classList.toggle("active");
     let content = this.nextElementSibling;
-    //If showing, unshow. Else, if not showing, show
+    // If showing, unshow. Else, if not showing, show
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
