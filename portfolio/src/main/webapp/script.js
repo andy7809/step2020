81eaf64918cf16cc658f261647468e06277148fc
Andrew Wiedenmann <andrewwiedenmann1@gmail.com>
2020-06-01 13:54:47 -0500

    Pass clickEvent to event response function for more linear code

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,9 +13,12 @@ for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", respondToClick);
 }
 
-function respondToClick(){
-    let content = this.nextElementSibling;
-    // If showing, unshow. Else, if not showing, show
+function respondToClick(clickEvent){
+    //The element that was clicked
+    const clickedElement = clickEvent.target;
+    //The next element in the HTML, which is the content div containing the content in the collapsible
+    let content = clickedElement.nextElementSibling;
+    // If showing (display is block), hide. Else, set display to block.
     if (content.style.display === "block") {
         content.style.display = "none";
     } else {
