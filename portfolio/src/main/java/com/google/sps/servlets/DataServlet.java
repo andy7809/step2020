a42ecef53cad24cf2e99db0ea5ccd1727905b1dd
Andrew Wiedenmann <andrewwiedenmann1@gmail.com>
2020-06-02 22:19:46 +0000

    Finished js post request

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,20 +14,46 @@
 
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.stream.Collectors;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  private static final String content = "Hello Andrew";
+  private static List<String> msgArray = new ArrayList<String>();
+
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    msgArray.add("Msg #1");
+    msgArray.add("Msg #2");
+    msgArray.add("msg #3");
+
     response.setContentType("text/html;");
-    response.getWriter().println(content);
+    response.getWriter().println(objToJson(msgArray));
+  }
+
+  private static String objToJson(Object o){
+    Gson gson = new Gson();
+    String json = gson.toJson(o);
+    return json;
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    try{
+      String test = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+      System.out.println(test);
+    } catch(Exception e){
+      e.printStackTrace();
+    }
+    request.getParameter("text");
   }
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -3,15 +3,16 @@
  @author Andrew Wiedenmann
  */
 "use strict";
-const MSG_TARGET = "msg";
+const POST_TARGET = "post";
+const COMMENT_TARGET = "comment";
 
-// Get all buttons. coll is an array of html elements
+// Get all collapsibles. coll is an array of html elements
 let coll = document.getElementsByClassName("collapsible");
 let i;
 
 for (i = 0; i < coll.length; i++) {
   // Add onclick functionality to every item in coll
-  coll[i].addEventListener("click", respondToClick);
+  coll[i].addEventListener("click", collapsibleRespondToClick);
 }
 
 /**
@@ -19,7 +20,7 @@ for (i = 0; i < coll.length; i++) {
  Responds to click by toggling the style.display property of the next element sibling in the DOM.
  @param clickEvent a clickEvent object on a collapsible item
  */
-function respondToClick(clickEvent) {
+function collapsibleRespondToClick(clickEvent) {
   //The element that was clicked
   const clickedElement = clickEvent.target;
   //The next element in the HTML, which is the content div containing the content in the collapsible
@@ -30,4 +31,15 @@ function respondToClick(clickEvent) {
   } else {
     content.style.display = "block";
   }
+}
+
+let submitButton = document.getElementById(POST_TARGET);
+submitButton.addEventListener("click", postComment);
+
+function postComment(clickEvent){
+  const data = {'cont': document.getElementById(COMMENT_TARGET).value};
+  fetch("/data", {
+    method: "POST", 
+    body: JSON.stringify(data)
+  });
 }
