dee5dae86bc22dad482497ae4bf1370388c50fdf
Andrew Wiedenmann <andrewwiedenmann1@gmail.com>
2020-06-11 19:24:49 +0000

    Add onchange listener to select element

--- portfolio/src/main/webapp/scripts/main.dart
+++ portfolio/src/main/webapp/scripts/main.dart
@@ -6,6 +6,9 @@ import 'dart:convert';
 
 final String collapsibleQueryStr = ".collapsible";
 final String postBtnQueryStr = "#post";
+final String commentQueryStr = "#comment";
+final String commentWrapperQueryString = "#comment-wrapper";
+final String selectQueryStr = "#cmnt-display-num";
 
 void main() {
   // Gets all collapsibles
@@ -14,8 +17,12 @@ void main() {
 
   var submitBtn = querySelector(postBtnQueryStr);
   submitBtn.onClick.listen(submitComment);
+  submitBtn.onClick.listen(displayComments);
 
   window.onLoad.listen(displayComments);
+
+  var selectElement = querySelector(selectQueryStr);
+  selectElement.onChange.listen(displayComments);
 }
 
 // Adds the handleCollapsibleClick handler to an element
@@ -36,7 +43,7 @@ void handleCollapsibleClick(Event event) {
 
 // Handles a click on the submit button
 void submitComment(Event event) {
-  var commentTextArea = querySelector("#comment") as TextAreaElement; 
+  var commentTextArea = querySelector(commentQueryStr) as TextAreaElement; 
   var json = { 'comment': commentTextArea.value };
   var request = new HttpRequest();
   request.open("POST", "/data");
@@ -50,7 +57,7 @@ Future<void> displayComments(Event event) async {
   var responseJson = jsonDecode(comments);
   print(responseJson);
 
-  var commentWrapper = querySelector("#comment-wrapper");
+  var commentWrapper = querySelector(commentWrapperQueryString);
   for(final comment in responseJson["commentList"]) {
     if (comment["content"].length > 0) {
       var commentDiv = new DivElement();
@@ -62,7 +69,7 @@ Future<void> displayComments(Event event) async {
 }
 
 String getNumberOfCommentsToDisplay() {
-  var selectElement = querySelector(SELECT_QUERY_STR) as SelectElement;
+  var selectElement = querySelector(selectQueryStr) as SelectElement;
   var selectedItem = selectElement.selectedOptions;
   return selectedItem[0].value;
 }
