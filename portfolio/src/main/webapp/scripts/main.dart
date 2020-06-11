5261fbdf5199f12d2fa65330c3c022c4de2ea7e7
Andrew Wiedenmann <andrewwiedenmann1@gmail.com>
2020-06-11 17:14:17 +0000

    change final var names to camelCase

--- portfolio/src/main/webapp/scripts/main.dart
+++ portfolio/src/main/webapp/scripts/main.dart
@@ -4,16 +4,21 @@
 import 'dart:html';
 import 'dart:convert';
 
+<<<<<<< HEAD
 final String COLLAPSIBLE_QUERY_STR = ".collapsible";
 final String POST_BTN_QUERY_STR = "#post";
 final String SELECT_QUERY_STR = "#cmnt-display-num";
+=======
+final String collapsibleQueryStr = ".collapsible";
+final String postBtnQueryStr = "#post";
+>>>>>>> f0469ce... change final var names to camelCase
 
 void main() {
   // Gets all collapsibles
-  var collapsibles = querySelectorAll(COLLAPSIBLE_QUERY_STR);
+  var collapsibles = querySelectorAll(collapsibleQueryStr);
   collapsibles.forEach(addCollapsibleClickListener);
 
-  var submitBtn = querySelector(POST_BTN_QUERY_STR);
+  var submitBtn = querySelector(postBtnQueryStr);
   submitBtn.onClick.listen(submitComment);
 
   window.onLoad.listen(displayComments);
