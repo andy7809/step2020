6154fdab3460f711e969e92e64223cfba97ca174
Andrew Wiedenmann <andrewwiedenmann1@gmail.com>
2020-06-12 22:31:56 +0000

    Add name field to comment

--- portfolio/src/main/java/com/google/sps/servlets/Comment.java
+++ portfolio/src/main/java/com/google/sps/servlets/Comment.java
@@ -4,12 +4,15 @@ package com.google.sps.servlets;
 */
 public class Comment{
   private String content;
+  private String name;
 
   /**
   * Creates a new instance of a Comment from a string
   * @param content The string content of this comment
+  * @param name The name of the user posting this comment
   */
-  public Comment(String content){
+  public Comment(String content, String name){
     this.content = content;
+    this.name = name;
   }
 }
